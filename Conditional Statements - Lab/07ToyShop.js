a5c84a0d14a66f3ca78b7a66c113548e94207626
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-05-03 16:09:05 +0100

    Update 07ToyShop.js

--- Conditional Statements - Lab/07ToyShop.js
+++ Conditional Statements - Lab/07ToyShop.js
@@ -1,37 +1,35 @@
 function toyShop(input){
 let tripPrice = Number(input[0]);
-let puzelsNum = Number[input[1]];
+let puzelsNum = Number(input[1]);
 let dollsNum = Number(input[2]);
 let tedyNum = Number(input[3]);
 let minionNum = Number(input[4]);
 let trucksNum = Number(input[5]);
 
-let puzelsPrice = puzelsNum * 2.6;
-let dollsPrice = dollsNum * 3.0;
-let tedyPrice = tedyNum * 4.10;
-let minionPrice = minionNum * 8.20;
-let truckPrice = trucksNum * 2;
+let puzelsPrice = 2.6;
+let dollsPrice = 3.0;
+let tedyPrice = 4.10;
+let minionPrice = 8.20;
+let truckPrice = 2.0;
 
-let toyNums = puzelsNum + dollsNum + tedyNum + minionNum + trucksNum;
-let totalMoney = puzelsPrice*puzelsNum + dollsPrice*dollsNum + tedyPrice*tedyNum + minionPrice*minionNum + truckPrice*trucksNum;
+let toyNums =puzelsNum + dollsNum + tedyNum + minionNum + trucksNum;
+let totalMoney = 2.6*puzelsNum + 3.0*dollsNum + 4.10*tedyNum + 8.20*minionNum + 2.0*trucksNum;
 
-if(toyNums >= 50 ){
-totalMoney *= 0.75;
+if (toyNums >= 50)
+{
+ totalMoney = totalMoney * 0.75;
+}
+totalMoney*=0.9;
+
+
+ if (totalMoney >= tripPrice){
+     let diff = totalMoney - tripPrice;
+    console.log(`Yes! ${diff.toFixed(2)} lv left.`)
+ }
+ else {
+    diff = tripPrice - totalMoney;
+   console.log(`Not enough money! ${diff.toFixed(2)} lv needed.`)
+ }
 }
 
-let result = totalMoney * 0.90 - tripPrice;
-
- if(result >= 0){
-console.log(`Yes! ${result} lv left.`)
- }
- else
- {
-console.log(`Not enough money! ${Math.abs(result)} lv needed.`)
- }
-}
-toyShop([40.8,
-    20,
-    25,
-    30,
-    50,
-    10])
+toyShop(["40.8","20","25","30","50","10"])
