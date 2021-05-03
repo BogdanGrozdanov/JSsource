8cfc2663a0bcd876b70e8bd027eed9fe39f7385a
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-05-03 20:26:11 +0100

    Update 04MetricConverter.js

--- Conditional Statements -Exercise/04MetricConverter.js
+++ Conditional Statements -Exercise/04MetricConverter.js
@@ -1,3 +1,38 @@
 function metricConverter(input){
+let value = Number(input[0]);
+let inMesure = input[1];
+let outMesure = input[2];
+let outValue = 0.0;
 
+if (inMesure === "mm"){
+  if (outMesure === "cm"){
+    outValue = value * 0.01;
+  }
+  else if (outMesure === "m")
+  {
+    outValue = value * 0.001;
+  }
 }
+else if (inMesure === "cm")
+{
+    if (outMesure === "mm"){
+        outValue = value * 10;
+    }
+    else if (outMesure === "m")
+    {
+        outValue = value * 0.001;
+    }
+}
+else if (inMesure === "m")
+{
+   if (outMesure === "mm"){
+        outValue = value * 1000;
+   }
+   else if(outMesure === "cm")
+   {
+     outValue = value * 100
+   }
+}
+console.log(outValue.toFixed(3));
+}
+metricConverter(["1204.34","mm","cm"])
