9751cf5951cd7757f2abacb36166a14f65123349
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-05-09 09:43:20 +0100

    Create 02.Weekend or Working Day.js

---
+++ Conditional Statements Advanced - Lab/02.Weekend or Working Day.js
@@ -0,0 +1,21 @@
+function weekendOrWorkingDay(input){
+    let day = input[0];
+    switch (day){
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+            output = "Working day";
+            break;
+        case "Saturday":
+        case "Sunday":
+            output = "Weekend";
+            break; 
+        default:
+            output = "Error";
+         break;                       
+    }
+    console.log(output)
+}
+weekendOrWorkingDay(["Friday"])
