02114c9de6df4e96d0ecba39fe1a24e7bfb207a9
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-05-10 20:01:51 +0100

    Create 10. Invalid Number.js

---
+++ Conditional Statements Advanced - Lab/10. Invalid Number.js
@@ -0,0 +1,13 @@
+function invalidNumber(input){
+    let number = Number(input[0])
+
+    if (number === 0|| number >=100 && number <= 200)
+    {
+
+    }
+    else
+    {
+        console.log("invalid")
+    }
+}
+invalidNumber(["2"])
